80a8052a54355beeea2aa37c46861468cbf52a48
danye16 <danyel.parracarrillo@gmail.com>
2023-09-29 09:03:09 -0700

    Agregar archivos de proyecto.

---
+++ Examen.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    internal class Examen
+    {
+        public void Intervalo(int numero1, int numero2)
+        {
+            if (numero1 == numero2)
+                Console.WriteLine("Los numeros ingrersados son iguales");
+
+            if (numero1 < numero2)
+            {
+                for (int c = numero1; c <= numero2; c++)
+                {
+                    Console.WriteLine($"La secuenta de los numeros es {c}");
+                }
+            }
+            else if (numero1 > numero2)
+            {
+                for (int c = numero1; c >= numero2; c--)
+                {
+
+
+                    Console.WriteLine($"La secuenta de los numeros es {c}");
+
+                }
+
+            }
+        }
+    }   
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Examen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Examen _examen = new Examen();
+
+            Console.WriteLine("Escriba dos numeros");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+
+
+            _examen.Intervalo(numero1, numero2);
+        }
+    }
+}
